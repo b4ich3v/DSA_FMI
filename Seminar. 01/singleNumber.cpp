78979f1667fa6750a67eef3910734e0182a04216
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-14 22:27:45 +0300

    Create singleNumber.cpp

---
+++ Seminar. 01/singleNumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+
+class Solution
+{
+public:
+
+    int singleNumber(std::vector<int>& nums)
+    {
+
+        int res = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+
+            res ^= nums[i];
+
+        }
+
+        return res;
+
+    }
+
+};
