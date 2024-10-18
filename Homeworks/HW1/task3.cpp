88c0ee6303a484ea14bfeb4cf5ae8215bce8aed5
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-18 14:54:28 +0300

    Update task3.cpp

--- Homeworks/HW1/task3.cpp
+++ Homeworks/HW1/task3.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+struct Student 
+{
+
+    std::string name;
+    int score;
+
+    bool operator < (const Student& other) const 
+    {
+        
+        if (score != other.score) return score > other.score;
+        return name < other.name; 
+
+    }
+
+};
+
+int main() 
+{
+
+    int N;
+    std::cin >> N;
+
+    std::vector<Student> students(N);
+
+    for (int i = 0; i < N; i++) 
+    {
+
+        std::cin >> students[i].name;
+
+    }
+
+    for (int i = 0; i < N; i++) 
+    {
+
+        std::cin >> students[i].score;
+
+    }
+
+    std::sort(students.begin(), students.end());
+
+    for (const auto& student : students) 
+    {
+
+        std::cout << student.name << " " << student.score << std::endl;
+
+    }
+
+    return 0;
+
+}
 
