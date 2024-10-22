fefaaf926b9b50979d99907226917cbeb4dfe058
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-22 23:01:19 +0300

    Update exam1.cpp

--- Exams/Exam. 01/exam1.cpp
+++ Exams/Exam. 01/exam1.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution 
+{
+public:
+
+	void printSpecialSorted(const std::vector<int>& arr)
+	{
+
+		int N = arr.size();
+		int K = *max_element(arr.begin(), arr.end()) + 1;
+		std::vector<int> counter(K, 0);
+
+		for (int i = 0; i < N; i++) counter[arr[i]]++;
+
+		while (true)
+		{
+
+			bool hasElements = false;
+
+			for (int i = 0; i < K; i++)
+			{
+
+				if (counter[i] != 0)
+				{
+
+					std::cout << i << " ";
+					counter[i]--;
+					hasElements = true;
+
+				}
+
+			}
+
+			if (!hasElements) break;
+			std::cout << std::endl;
+
+		}
+
+	}
+
+};
+
 
