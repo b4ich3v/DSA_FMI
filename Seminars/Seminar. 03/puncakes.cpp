ac0222d5f457f81585736dcad305f1560d3b98ce
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-10-27 14:39:45 +0200

    Update puncakes.cpp

--- Seminars/Seminar. 03/puncakes.cpp
+++ Seminars/Seminar. 03/puncakes.cpp
@@ -2,40 +2,58 @@
 #include <vector>
 #include <algorithm>
 
-int main() 
+bool enough(const std::vector<long long>& array, long long time, long long N)
 {
 
-    int N, K;
-    std::cin >> N >> K;
+    long long counter = 0;
+    long long size = array.size();
 
-    std::vector<int> times(K);
+    for (long long i = 0; i < size; i++)
+    {
 
-    for (int i = 0; i < K; i++) std::cin >> times[i];
+        counter += time / array[i];
+        if (counter >= N) return true;
+
+    }
+
+    return counter >= N;
+
+}
+
+long long calculateTime(const std::vector<long long>& array, long long N)
+{
 
     long long left = 1;
-    long long right = (long long)*std::min_element(times.begin(), times.end()) * N;
-    long long answer = right;
+    long long right = *max_element(array.begin(), array.end()) * N;
+    long long neededTime = right;
 
     while (left <= right) 
     {
 
-        long long mid = left + (right - left) / 2;
-        long long pancakesMade = 0;
+        long long middle = left + (right - left) / 2;
 
-        for (int i = 0; i < K; i++) 
-        {
-
-            pancakesMade += mid / times[i];
-            if (pancakesMade >= N) break;
-
-        }
-
-        if (pancakesMade >= N) { answer = mid; right = mid - 1; }
-        else left = mid + 1;
+        if (enough(array, middle, N)) { neededTime = middle; right = middle - 1; }
+        else left = middle + 1;
 
     }
 
-    std::cout << answer << std::endl;
+    return neededTime;
+
+}
+
+int main() 
+{
+
+    long long N = 0;
+    long long K = 0;
+
+    std::cin >> N >> K;
+
+    std::vector<long long> array(K);
+    for (long long i = 0; i < K; i++) std::cin >> array[i];
+
+    std::cout << calculateTime(array, N);
+
     return 0;
 
 }
