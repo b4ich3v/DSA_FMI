fa04f56f93bc1bd9f57668c7b114dee6ae07f3ca
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-02 01:49:06 +0200

    Update task.cpp

--- Previous_Years/Tests/2023-2024/Test. 01/task.cpp
+++ Previous_Years/Tests/2023-2024/Test. 01/task.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+
+    std::string str;
+    std::cin >> str;
+
+    int N = str.size();
+    std::vector<std::pair<int, int>> counter(500);
+
+    for (int i = 0; i < 500; i++)
+    {
+
+        counter[i].first = 0;
+        counter[i].second = -1;
+
+    }
+
+    for (int i = 0; i < N; i++) 
+    {
+
+        counter[(int)str[i]].first++; 
+        counter[(int)str[i]].second = i;
+       
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+
+        if(counter[(int)str[i]].first == 1) std::cout << counter[(int)str[i]].second << " ";
+
+    }
+
+    return 0;
+
+}
 
