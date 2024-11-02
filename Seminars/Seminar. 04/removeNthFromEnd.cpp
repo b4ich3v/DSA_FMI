4ba6e96fe7d12f83561f07c73aa3532a8f6e44ae
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-03 00:39:22 +0200

    Create removeNthFromEnd.cpp

---
+++ Seminars/Seminar. 04/removeNthFromEnd.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+struct ListNode 
+{
+
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+
+};
+ 
+
+class Solution 
+{
+public:
+
+    ListNode* removeNthFromEnd(ListNode* head, int n) 
+    {
+
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        for (int i = 0; i < n; i++)  fast = fast->next;
+
+
+        while (fast->next)
+        {
+
+            slow = slow->next;
+            fast = fast->next;
+
+        }
+
+        ListNode* oper = slow->next;
+        slow->next = slow->next->next;
+        delete oper;
+
+        return head;
+
+    }
+
+};
