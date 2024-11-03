3aae07a73b009401b0147f07fe65940c775ef0cd
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-03 21:38:54 +0200

    Update binarySearch.cpp

--- Algorithms/Searching/binarySearch.cpp
+++ Algorithms/Searching/binarySearch.cpp
@@ -1,18 +1,15 @@
-int binarySearch(const std::vector<int>& arr, int target) 
+int binarySearch(const std::vector<int>& arr, int left, int right, int target)
 {
 
-    int left = 0;
-    int right = arr.size() - 1;
-
-    while (left <= right) 
+    while (left <= right)
     {
 
-        int middle = left + (right - left) / 2; 
+        int middle = left + (right - left) / 2;
 
         if (arr[middle] > target) right = middle - 1;
         else if (arr[middle] < target) left = middle + 1;
         else  return middle;
-            
+
     }
 
     return -1;
