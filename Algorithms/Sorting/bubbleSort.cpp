7335d6a52c88f9371111b932ac848eb3f0387c21
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-04 00:42:03 +0200

    Update bubbleSort.cpp

--- Algorithms/Sorting/bubbleSort.cpp
+++ Algorithms/Sorting/bubbleSort.cpp
@@ -1,1 +1,16 @@
+void bubbleSort(std::vector<int>& arr) 
+{
 
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+
+        for (int j = 0; j < arr.size() - i - 1; j++)
+        {
+
+            if (arr[j] > arr[j + 1]) std::swap(arr[j], arr[j + 1]);
+
+        }
+
+    }
+
+}
