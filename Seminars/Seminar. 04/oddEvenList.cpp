c2e1960825ee0fbf263741d97d566fc8bdbfbdf5
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-04 16:13:24 +0200

    Update oddEvenList.cpp

--- Seminars/Seminar. 04/oddEvenList.cpp
+++ Seminars/Seminar. 04/oddEvenList.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 struct ListNode 
 {
@@ -10,33 +12,70 @@ struct ListNode
     ListNode(int x, ListNode* next) : val(x), next(next) {}
 
 };
- 
-class Solution 
+
+class Solution
 {
 public:
 
-    ListNode* oddEvenList(ListNode* head) 
+    int sizeOfList(ListNode* head)
     {
-        
-        if (!head || !head->next)  return head;
 
-        ListNode* odd = head;          
-        ListNode* even = head->next;    
-        ListNode* evenHead = even;      
+        ListNode* current = head;
+        int counter = 0;
 
-        while (even && even->next) 
+        while (current)
         {
 
-            odd->next = even->next;     
-            odd = odd->next;             
-            even->next = odd->next;      
-            even = even->next;  
-           
+            current = current->next;
+            counter++;
+
         }
 
-        odd->next = evenHead;           
-        return head;   
-        
+        return counter;
+
+    }
+
+    ListNode* oddEvenList(ListNode* head)
+    {
+
+        if (!head || !head->next) return head;  
+
+        int size = sizeOfList(head);
+
+        ListNode** nodes = new ListNode * [size];
+        ListNode* current = head;
+
+        for (int i = 0; i < size; i++)
+        {
+
+            nodes[i] = current;
+            current = current->next;
+
+        }
+
+        ListNode* result = nodes[0];
+        current = result;
+
+        for (int i = 0; i < size; i += 2)
+        {
+
+            current->next = nodes[i];
+            current = current->next;
+
+        }
+
+        for (int i = 1; i < size; i += 2)
+        {
+
+            current->next = nodes[i];
+            current = current->next;
+
+        }
+
+        current->next = nullptr;
+        delete[] nodes;
+        return result;
+
     }
 
 };
