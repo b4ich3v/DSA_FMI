effd33bfd8220ffaabba512c568cb62fc1c85988
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-06 01:54:12 +0200

    Update Stack.hpp

--- Structures/Stack.hpp
+++ Structures/Stack.hpp
@@ -1,1 +1,59 @@
+#include <stdexcept>
 
+template <typename T>
+class Stack 
+{
+public:
+   
+    static const size_t MAX = 100;
+    static const int EMPTY_STACK = -1;
+
+private:
+
+    T data[MAX];
+
+    int top;
+
+    bool full() const 
+    {
+
+        return top == MAX - 1;
+
+    }
+
+public:
+
+    Stack() : top(EMPTY_STACK) {}
+
+    bool empty() const 
+    {
+
+        return top == EMPTY_STACK;
+
+    }
+
+    void push(T const& x) 
+    {
+
+        if (full()) throw std::runtime_error("Error");  
+        data[++top] = x;
+
+    }
+
+    T pop() 
+    {
+
+        if (empty()) throw std::runtime_error("Error");
+        return data[top--];
+
+    }
+
+    T const& peek() const 
+    {
+
+        if (empty()) throw std::runtime_error("Error");
+        return data[top];
+
+    }
+
+};
