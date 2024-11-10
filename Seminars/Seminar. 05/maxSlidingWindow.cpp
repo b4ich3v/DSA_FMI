eeddc74a6e101055a34cc64880edfddaa70bb86e
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-10 21:16:10 +0200

    Update maxSlidingWindow.cpp

--- Seminars/Seminar. 05/maxSlidingWindow.cpp
+++ Seminars/Seminar. 05/maxSlidingWindow.cpp
@@ -1,6 +1,53 @@
 #include <vector>
-#include <deque>
+#include <queue>
 #include <iostream>
+#include <deque>
+#include <limits.h>
+
+class MaxQueue
+{
+private:
+
+    std::queue<int> dataQueue;  
+    std::deque<int> maxQueue;   
+
+public:
+
+    MaxQueue() {}
+
+    void push(int val)
+    {
+
+        dataQueue.push(val);
+
+        while (!maxQueue.empty() && maxQueue.back() < val) maxQueue.pop_back();
+        maxQueue.push_back(val);
+
+    }
+
+    void pop()
+    {
+
+        if (dataQueue.front() == maxQueue.front()) maxQueue.pop_front();
+        dataQueue.pop();
+
+    }
+
+    int front()
+    {
+
+        return dataQueue.front();
+
+    }
+
+    int getMax()
+    {
+
+        return maxQueue.front();
+
+    }
+
+};
 
 class Solution
 {
@@ -9,19 +56,18 @@ public:
     std::vector<int> maxSlidingWindow(std::vector<int>& nums, int k)
     {
 
-        std::deque<int> d;
+        MaxQueue q;
         std::vector<int> result;
+  
+        for (int i = 0; i < k; i++) q.push(nums[i]);
+      
+        result.push_back(q.getMax());
 
-        for (int i = 0; i < nums.size(); ++i)
+        for (int i = k; i < nums.size(); i++)
         {
-
-            if (!d.empty() && d.front() == i - k)  d.pop_front();
-
-            while (!d.empty() && nums[d.back()] < nums[i])  d.pop_back();
-
-            d.push_back(i);
-
-            if (i >= k - 1) result.push_back(nums[d.front()]);
+            q.pop();          
+            q.push(nums[i]); 
+            result.push_back(q.getMax()); 
 
         }
 
