524b2bcbadd9c561e6e72fd0b1d78d5df32b02c0
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-13 01:49:17 +0200

    Create preorderTraversal.cpp

---
+++ Seminars/Seminar. 06/preorderTraversal.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <iostream>
+
+
+struct TreeNode 
+{
+
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+ 
+class Solution 
+{
+public:
+
+   
+
+    void pushingElements(TreeNode* root, std::vector<int>& result)
+    {
+
+        if (!root) return;
+
+        result.push_back(root->val);
+
+        pushingElements(root->left, result);
+        pushingElements(root->right, result);
+
+    }
+
+    std::vector<int> preorderTraversal(TreeNode* root)
+    {
+
+        std::vector<int> result;
+        pushingElements(root, result);
+
+        return result;
+
+    }
+
+};
