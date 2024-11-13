f89da2591fa0b42845bf9f01301dacae5a88476d
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-13 23:52:19 +0200

    Create searchBST.cpp

---
+++ Seminars/Seminar. 06/searchBST.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <string>
+#include <stack>
+#include <algorithm>
+
+struct TreeNode 
+{
+
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+
+class Solution 
+{
+public:
+
+    TreeNode* searchBST(TreeNode* root, int val)
+    {
+
+        while (root) 
+        {
+
+            if (root->val == val) return root;
+            else if (root->val < val) root = root->right;
+            else if (root->val > val)  root = root->left;
+               
+        }
+
+
+        return root;
+
+    }
+
+};
