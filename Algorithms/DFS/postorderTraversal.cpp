45b08d7fa50aff9f0eec0ae811c90f9392f83138
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-15 17:54:36 +0200

    Create postorderTraversal.cpp

---
+++ Algorithms/DFS/postorderTraversal.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <set>
+
+struct TreeNode
+{
+
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+
+class Solution
+{
+public:
+        
+    void pushFunc(TreeNode* node, std::vector<int>& arr) 
+    {
+
+        if (!node) return;
+
+        pushFunc(node->left, arr);
+        pushFunc(node->right, arr);
+        arr.push_back(node->val);
+
+    }
+
+    std::vector<int> postorderTraversal(TreeNode* root)
+    {
+
+        std::vector<int> result;
+
+        pushFunc(root, result);
+        return result;
+
+    }
+
+};
