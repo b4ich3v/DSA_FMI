91094e3012b81d70b969b711530636d23c03ce28
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-15 17:53:36 +0200

    Update postorderTraversal.cpp

--- Seminars/Seminar. 06/postorderTraversal.cpp
+++ Seminars/Seminar. 06/postorderTraversal.cpp
@@ -1,3 +1,23 @@
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <set>
+
+struct TreeNode
+{
+
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+
 class Solution
 {
 public:
