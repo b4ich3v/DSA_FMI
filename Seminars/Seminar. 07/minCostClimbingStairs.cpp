c61858132be6c3a00500c1ee8f931d90c612a32c
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-21 14:08:35 +0200

    Update minCostClimbingStairs.cpp

--- Seminars/Seminar. 07/minCostClimbingStairs.cpp
+++ Seminars/Seminar. 07/minCostClimbingStairs.cpp
@@ -1,34 +1,27 @@
 #include <vector>
-#include <stack>
-#include <iostream>
 #include <algorithm>
-#include <queue>
+#include <string>
 
 class Solution
 {
 public:
 
-    int minCostClimbingStairs(std::vector<int>& cost)
+    int minCostClimbingStairs(std::vector<int>& cost) 
     {
 
-        int size = cost.size();
+        std::vector<int> dp(cost.size(), 0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
 
-
-        int prev2 = 0; 
-        int prev1 = 0; 
-
-        for (int i = 2; i <= size; i++) 
+        for (int i = 2; i < cost.size(); i++)
         {
-            
-            int current = std::min(prev1 + cost[i - 1], prev2 + cost[i - 2]);
-            prev2 = prev1;
-            prev1 = current;
+
+            dp[i] = std::min(dp[i - 1], dp[i - 2]) + cost[i] ;
 
         }
 
-        return prev1;
+        return std::min(dp[cost.size() - 1], dp[cost.size() - 2]);
 
     }
 
 };
-
