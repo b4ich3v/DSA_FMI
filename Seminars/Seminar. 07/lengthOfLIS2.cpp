0c068e007bced6258e48910b246af17f2d2c6b2c
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-22 15:22:30 +0200

    Update lengthOfLIS2.cpp

--- Seminars/Seminar. 07/lengthOfLIS2.cpp
+++ Seminars/Seminar. 07/lengthOfLIS2.cpp
@@ -2,16 +2,16 @@
 #include <string>
 #include <vector>
 
-class Solution 
+class Solution
 {
 public:
 
-    int longestCommonSubstringInOrder(std::string text1, std::string text2)
+    int lengthOfLIS2(std::string text1, std::string text2)
     {
 
         int rows = text1.size();
         int cols = text2.size();
-        int result = 0;  
+        int result = 0;
         std::vector<std::vector<int>> dp(rows + 1, std::vector<int>(cols + 1, 0));
 
         for (int i = 1; i <= rows; i++)
@@ -20,11 +20,11 @@ public:
             for (int j = 1; j <= cols; j++)
             {
 
-                if (text1[i - 1] == text2[j - 1]) 
+                if (text1[i - 1] == text2[j - 1])
                 {
 
-                    dp[i][j] = dp[i - 1][j - 1] + 1;  
-                    result = std::max(result, dp[i][j]);  
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    result = std::max(result, dp[i][j]);
 
                 }
                 else dp[i][j] = 0;
@@ -38,5 +38,3 @@ public:
     }
 
 };
-
-
