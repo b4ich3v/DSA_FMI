e04ce2e944d895e015db11ce85c850964af2f4bd
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-11-27 23:59:30 +0200

    Update exponentialSearch.cpp

--- Algorithms/Searching/exponentialSearch.cpp
+++ Algorithms/Searching/exponentialSearch.cpp
@@ -1,15 +1,1 @@
-int exponentialSearch(const std::vector<int>& arr, int target)
-{
 
-    int size = arr.size();
-    if (arr[0] == target) return 0;
-       
-    int i = 1;
-    while (i < size && arr[i] <= target) i *= 2;
-       
-    int left = i / 2;
-    int right = std::min(i, size - 1);
-
-    return binarySearch(arr, left, right, target);
-
-}
