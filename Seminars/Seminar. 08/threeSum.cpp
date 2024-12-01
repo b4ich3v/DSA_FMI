da0f890c7113817480e241f433776c3483187a63
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-12-01 22:45:29 +0200

    Update threeSum.cpp

--- Seminars/Seminar. 08/threeSum.cpp
+++ Seminars/Seminar. 08/threeSum.cpp
@@ -1,49 +1,48 @@
+#include <string>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
 
-class Solution
+class Solution 
 {
 public:
 
     std::vector<std::vector<int>> threeSum(std::vector<int>& nums)
     {
+        if (nums.size() < 3) return {};
 
-        std::vector<std::vector<int>> result;
         std::sort(nums.begin(), nums.end());
+        std::unordered_map<int, int> mp;
 
-        for (int i = 0; i < nums.size(); i++)
+        for (int i = 0; i < nums.size(); i++) mp[nums[i]] = i;
+
+        std::vector<std::vector<int>> triplets;
+
+        for (int i = 0; i < nums.size(); i++) 
         {
-            
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int target = -nums[i];
-            int left = i + 1, right = nums.size() - 1;
 
-            while (left < right)
+            for (int j = i + 1; j < nums.size(); j++) 
             {
 
-                int sum = nums[left] + nums[right];
-
-                if (sum == target)
+                if (mp.count(target - nums[j]) && mp[target - nums[j]] > j)
                 {
 
-                    result.push_back({ nums[i], nums[left], nums[right] });
-
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
+                    triplets.push_back({ nums[i], nums[j], target - nums[j] });
 
                 }
-                else if (sum < target) left++;
-                else right--;
+
+                j = mp[nums[j]];
 
             }
 
+            i = mp[nums[i]];
+
         }
 
-        return result;
+        return triplets;
 
     }
 
