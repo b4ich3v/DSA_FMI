66796e9f29e9d66c91489df31f4c1743d986b184
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-12-13 01:45:42 +0200

    Create DFS.cpp

---
+++ Algorithms/GRAPHS/DFS.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <unordered_set>
+
+void dfs(int current, std::unordered_map<int, std::unordered_set<int>>& graph, std::vector<bool>& visited)
+{
+
+	visited[current] = true;
+
+	for (const int child : graph[current]) 
+	{
+
+		if (!visited[child]) 
+		{
+
+			visited[child] = true;
+			dfs(child, graph, visited);
+
+		}
+
+	}
+
+	std::cout << current << " ";
+
+}
