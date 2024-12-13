905ae6f23c8c0944a7c94f3830a9fe5d3284bff3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2024-12-13 02:44:16 +0200

    Update bfs.cpp

--- Seminars/Seminar. 010/bfs.cpp
+++ Seminars/Seminar. 010/bfs.cpp
@@ -6,73 +6,76 @@
 #include <queue>
 #include <unordered_set>
 
-std::vector<int> bfs(int n, int m, std::vector<std::vector<int>> edges, int s) 
+std::vector<int> bfs(int n, int m, std::vector<std::vector<int>> edges, int s)
 {
 
-    std::vector<std::vector<int>> graph(n, std::vector<int>());
+	std::unordered_map<int, std::unordered_set<int>> graph;
+	int start = s - 1;
 
-    for (int i = 0; i < m; i++)
-    {
+	for (int i = 0; i < n; i++)
+	{
 
-        int node1 = edges[i][0];
-        int node2 = edges[i][1];
-        
-        graph[node1 - 1].push_back(node2 - 1);
-        graph[node2 - 1].push_back(node1 - 1);
+		graph[i];
 
-    }
+	}
 
-    std::vector<int> result(n, -1);
-    std::vector<bool> visited(n, false);
-    std::queue<int> q;
+	for (const auto& current : edges)
+	{
 
-    int start = s - 1;
-    q.push(start);
-    visited[start] = true;
-    result[start] = 0;
+		int node1 = current[0];
+		int node2 = current[1];
 
-    while (!q.empty()) 
-    {
+		graph[node1 - 1].insert(node2 - 1);
+		graph[node2 - 1].insert(node1 - 1);
 
-        int size = q.size();
+	}
 
-        for (int i = 0; i < size; i++)
-        {
+	std::queue<int> q;
+	std::unordered_set<int> visited;
+	std::vector<int> result(n, -1);
 
-            int current = q.front();
-            q.pop();
+	result[start] = 0;
+	visited.insert(start);
+	q.push(start);
 
-            const auto& children = graph[current];
+	while (!q.empty())
+	{
 
-            for (auto& child : children)
-            {
+		int size = q.size();
 
-                if (!visited[child])
-                {
+		for (int i = 0; i < size; i++)
+		{
 
-                    visited[child] = true;
-                    q.push(child);
-                    result[child] = result[current] + 6;
+			int current = q.front();
+			q.pop();
 
-                }
+			for (const auto& child : graph[current]) 
+			{
 
-            }
+				if (visited.find(child) == visited.end())
+				{
 
-        }
+					q.push(child);
+					visited.insert(child);
+					result[child] = result[current] + 6;
 
-    }
+				}
 
-    std::vector<int> finalResult;
+			}
 
-    for (int i = 0; i < n; i++)
-    {
+		}
 
-        if (i != start) finalResult.push_back(result[i]);
+	}
 
-    }
+	std::vector<int> finaltResult;
 
-    return finalResult;
+	for (int i = 0; i < n; i++)
+	{
+
+		if (i != start) finaltResult.push_back(result[i]);
+
+	}
+
+	return finaltResult;
 
 }
-
-
