04e8dd1910d7b444330eba1b4f1b03956b94edc3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-01-03 23:49:36 +0200

    Create bellman_ford.cpp

---
+++ Algorithms/GRAPHS/bellman_ford.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <iostream>
+
+struct Edge
+{
+public:
+
+	int from = 0;
+	int weight = 0;
+	int to = 0;
+
+};
+
+std::vector<int> dijkstra(int start, int countOfNodes, std::vector<Edge>& graph)
+{
+
+	std::vector<int> distances(graph.size(), INT_MAX);
+	distances[start] = 0;
+
+	for (int i = 0; i < countOfNodes - 1; i++)
+	{
+
+		for (const auto& current : graph)
+		{
+
+			if (distances[current.from] != INT_MAX && 
+				distances[current.from] + current.weight < distances[current.to]) 
+			{
+
+				distances[current.to] = distances[current.from] + current.weight;
+
+			}
+
+		}
+
+	}
+
+	for (const auto& current : graph) 
+	{
+
+		if (distances[current.from] != INT_MAX &&
+			distances[current.from] + current.weight < distances[current.to])
+		{
+
+			throw std::logic_error("Negative cycle");
+
+		}
+
+	}
+
+	return distances;
+
+}
